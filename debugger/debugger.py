80bd9bc56eb2e217561ac575ce88c53a1bdea27f
David Iserovich <david.iserovich@gmail.com>
2013-09-11 03:04:01 -0400

    Added disassembly command to shell, and some other fixes/tweaks

--- debugger/debugger.py
+++ debugger/debugger.py
@@ -12,13 +12,13 @@ kernel32 = windll.kernel32
 
 class Debugger():
     def __init__(self):
-        self.h_process          = None
-        self.pid                = None
-        self.debugger_active    = False
-        self.first_breakpoint   = True
-        self.breakpoints        = {}
-        self.event_callback     = None
-        self.bp_callback        = None    
+        self.h_process = None
+        self.pid = None
+        self.debugger_active = False
+        self.first_breakpoint = True
+        self.breakpoints = {}
+        self.event_callback = None
+        self.bp_callback = None    
         
     def set_event_callback(self, callback):
         """ Set method to be called upon all debugging events """
@@ -34,16 +34,16 @@ class Debugger():
         # set creation_flags = CREATE_NEW_CONSOLE if you want to see the GUI
         creation_flags = DEBUG_PROCESS
         
-        #instantiate the structs
+        # instantiate the structs
         startup_info = STARTUPINFO()
         process_info = PROCESS_INFORMATION()
         
-        startup_info.dwFlags        = 0x1
-        startup_info.wShowWindow    = 0x0
+        startup_info.dwFlags = 0x1
+        startup_info.wShowWindow = 0x0
         
-        startup_info.cb  = sizeof(startup_info)
+        startup_info.cb = sizeof(startup_info)
         
-        #http://msdn.microsoft.com/en-us/library/windows/desktop/ms682425%28v=vs.85%29.aspx
+        # http://msdn.microsoft.com/en-us/library/windows/desktop/ms682425%28v=vs.85%29.aspx
         if kernel32.CreateProcessA(path_to_exe,
                                   None,
                                   None,
@@ -58,10 +58,11 @@ class Debugger():
             print   "[*]  PID: %d" % process_info.dwProcessId
             
             # Obtain a handle to the open process
-            #self.h_process = self.open_process(process_info.dwProcessId)
+            # self.h_process = self.open_process(process_info.dwProcessId)
             
             self.debugger_active = True
             self.pid = process_info.dwProcessId
+            self.h_process = self.open_process(self.pid)
 
             return process_info.dwProcessId
             
@@ -73,7 +74,7 @@ class Debugger():
         # HANDLE WINAPI OpenProcess( dwDesiredAccess, bInheritHandle, dwProcessId)
         h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
         print "Opened pid: {}".format(pid)
-        #if h_process == 0:
+        # if h_process == 0:
         #    print "[*] Error opening process: %s" % kernel32.GetLastError()   
         return h_process
 
@@ -148,8 +149,8 @@ class Debugger():
         # BOOL WINAPI DebugActiveProcess(dwProcessId)
         success = kernel32.DebugActiveProcess(pid)
         if success:
-                self.debugger_active    = True
-                self.pid                = pid
+                self.debugger_active = True
+                self.pid = pid
                 return pid
                 
         else:
@@ -165,12 +166,15 @@ class Debugger():
         # Bytes read
         count_read = c_ulong(0)
         
-        if not kernel32.ReadProcessMemory(h_process, address, read_buf, length, byref(count_read)):
-            sys.stderr.write('Failed to read {} bytes of memory at address {}\n'.format(length, address))
+        result = kernel32.ReadProcessMemory(h_process, address, read_buffer, length, byref(count_read))
+        
+        if not result:
+            sys.stderr.write('Failed to read {} bytes of memory at address {} for process {}\n'.format(length, hex(address), h_process))
+            sys.stderr.write('Error: ' + hex(self.get_last_error()) + '\n')
             return None
         
         else:
-            data += read_buf.raw()
+            data += read_buffer.raw
             return data
         
     def write_process_memory(self, h_process, address, data):
@@ -189,7 +193,7 @@ class Debugger():
     def breakpoint_set(self, h_process, address):
         if not address in self.breakpoints:
             try:
-                #store the original byte
+                # store the original byte
                 original = self.read_process_memory(h_process, address, 1)
                 
                 # write the opcode for INT3, the breakpoint interrupt instruction
@@ -235,7 +239,7 @@ class Debugger():
             self.context = self.get_thread_context(h_thread=self.h_thread)
             self.context.Eip -= 1
             
-            kernel32.SetThreadContext(self.h_thread,byref(self.context))
+            kernel32.SetThreadContext(self.h_thread, byref(self.context))
             
         return DBG_CONTINUE
             
@@ -245,7 +249,7 @@ class Debugger():
             TODO:  Add callback arg for action on given event type
         """
         
-        debug_event     = DEBUG_EVENT()
+        debug_event = DEBUG_EVENT()
         continue_status = DBG_CONTINUE
         
         if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
@@ -255,10 +259,10 @@ class Debugger():
                                                                                   debug_event.dwProcessId,
                                                                                   debug_event.dwThreadId)
                 
-                debug_thread_context = self.get_thread_context(thread_id = debug_event.dwThreadId)
+                debug_thread_context = self.get_thread_context(thread_id=debug_event.dwThreadId)
                 
                 
-                #self.dump_thread_contexts()
+                # self.dump_thread_contexts()
                 
                 if DEBUG_EVENT_CODE_NAMES[debug_event.dwDebugEventCode] == 'EXCEPTION_DEBUG_EVENT':
                     print 'Exception Code: ', hex(debug_event.u.Exception.ExceptionRecord.ExceptionCode)
@@ -268,10 +272,15 @@ class Debugger():
                     continue_status = self.exception_handler_breakpoint(exception_address)
                     
                     if self.bp_callback:
-                        self.bp_callback()
+                        self.bp_callback(debug_event)
+                        
 
                 if self.event_callback:
                     self.event_callback()
+                    
+                # Stop debugging once the process exits
+                if DEBUG_EVENT_CODE_NAMES[debug_event.dwDebugEventCode] == 'EXIT_PROCESS_DEBUG_EVENT':
+                    self.debugger_active = False
                 
                 # BOOL WINAPI ContinueDebugEvent( dwProcessId, dwThreadId, dwContinueStatus)
                 print "Continuing", debug_event.dwProcessId, debug_event.dwThreadId             
@@ -302,7 +311,7 @@ class Debugger():
         threads = self.enumerate_threads(self.pid)
 
         for thread in threads:
-            thread_ctx = self.get_thread_context(thread_id = thread)
+            thread_ctx = self.get_thread_context(thread_id=thread)
 
             print "ThreadID: {}".format(thread)
             print "Instruction pointer RIP: {:016X}".format(thread_ctx.Rip)
--- debugger/shell.py
+++ debugger/shell.py
@@ -1,11 +1,18 @@
 import sys
 
+try:
+    from distorm3 import Decode, Decode16Bits, Decode32Bits, Decode64Bits
+except:
+    sys.err.write('Cannot load distorm3 disassembly library')
+
 class Shell():
     
     def __init__(self, debugger):
         self.debugger = debugger
     
-    def prompt(self):
+    def prompt(self, debug_event):
+        self.debug_event = debug_event
+        
         while True:
             input = raw_input('> ')
             command = input.split()[0] if input.split() else None
@@ -18,7 +25,8 @@ class Shell():
                     pass
                 
             if f:
-                return f(command)
+                if f(command):
+                    return
                 
             else:
                 sys.stderr.write('Bad command. Try q to detach and quit, or n to continue\n')
@@ -33,4 +41,14 @@ class Shell():
     
     def n(self, command):
         """Next - Continue execution"""
-        return
+        return True
+    
+    def d(self, command):
+        try:
+            l = Decode(self.debug_event.u.Exception.ExceptionRecord.ExceptionAddress, 
+                       self.debugger.read_process_memory(self.debugger.h_process, self.debug_event.u.Exception.ExceptionRecord.ExceptionAddress, 32),
+                       Decode64Bits)
+            for i in l:
+                print "0x%08x (%02x) %-20s %s" % (i[0],  i[1],  i[3],  i[2])
+        except Exception as e:
+            sys.stderr.write('A problem occured in disassembling nearby memory {}\n Do you have distorm3 installed?\n'.format(e))
